1c982ab5137246162a140b91df72499be905ae01
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2022-01-07 00:36:05 +0900

    Add text search feature

--- api.rb
+++ api.rb
@@ -7,6 +7,7 @@ class Api
         @http.use_ssl = true
         token = File.read(".token/#{host}")
         @header = {"Authorization" => "Bearer #{token}"}
+    
     end
 
     def search (username)
@@ -18,8 +19,20 @@ class Api
         params = {limit: 100}
         params[:max_id] = max_id unless max_id.nil?
         query = URI.encode_www_form(params)
-        response = @http.get("/api/v1/accounts/#{user_id}/statuses?#{query}", @header)
-        statuses = JSON.parse(response.body)
+        statuses = get("/api/v1/accounts/#{user_id}/statuses?#{query}")
         statuses.filter{|e| e['reblog'].nil? }.map{|s| {'id' => s['id'], 'content' => s['content']}}
+    end
+
+    def get_timelines(max_id = nil)
+        params = {limit: 100, local: 1}
+        params[:max_id] = max_id unless max_id.nil?
+        query = URI.encode_www_form(params)
+        statuses = get("/api/v1/timelines/public?#{query}")
+        statuses.filter{|s| s['reblog'].nil? }.map{|s| {id: s['id'], content: s['content']}}
+    end
+
+    def get(endpoint)
+        response = @http.get(endpoint, @header)
+        JSON.parse(response.body)
     end
 end
